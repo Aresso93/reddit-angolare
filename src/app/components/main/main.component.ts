e58cbaea769949f6031543e29a2b8f07ccac9943
Aresso93 <ares.fiumicelli@gmail.com>
2023-09-15 17:01:15 +0200

    /

--- src/app/components/main/main.component.ts
+++ src/app/components/main/main.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { DataService } from 'src/app/services/data.service';
 import { RedditService } from 'src/app/services/reddit.service';
+import { Post } from 'src/app/model/post';
 
 @Component({
   selector: 'app-main',
@@ -8,8 +9,17 @@ import { RedditService } from 'src/app/services/reddit.service';
   styleUrls: ['./main.component.scss']
 })
 export class MainComponent {
+post: any;
   constructor(private dataServ: DataService, private reddit: RedditService){
 
     reddit.getPosts().then(posts => console.log(posts));
   }
+
+  ngOnInit(): void {
+    this.getPosts();
+  }
+  getPosts() {
+    throw new Error('Method not implemented.');
+  }
+
 }
